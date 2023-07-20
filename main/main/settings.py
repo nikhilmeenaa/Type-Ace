42da1671c8a21023d6e1d5f2fd97ced665c3e131
Nikhil Meena <108169212+nikhilmeenaa@users.noreply.github.com>
2023-07-21 00:27:54 +0530

    Update settings.py
    static files error dealing

--- main/main/settings.py
+++ main/main/settings.py
@@ -140,3 +140,21 @@ STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+import os
+STATIC_URL = 'static/'
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ALLOWED_HOSTS = ['*']
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = (
+    BASE_DIR/'static',
+)
